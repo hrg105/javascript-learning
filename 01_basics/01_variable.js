d2a68a8fc9a1402109fc325026abaf6e0b2dd3a0
hrg105 <133035977+hrg105@users.noreply.github.com>
2024-02-23 16:22:11 +0000

    code config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=144
+let accountEmail="anand@gmail.com"
+var accountPassword="12345"  // better to not use
+accountCity="dehradun" //not good to initialize like it
+// accountId=244 //not allowed
+accountEmail="anand1@gmail.com"
+accountPassword="24445"
+accountCity="delhi"
+let accountState;
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all code as newer version
+// alert( 3 + 3) // we are using node js not browser
+let name="anand"
+let age=22
+let IsloggedIn=false
+
+//number=>2 to the power 53
+//bigint
+//string=>""
+// boolean=>true/false
+//null=>standalone value
+// undefined=>
+//symbol=>unique
+
+
+//object
+console.log(typeof"anand ")
